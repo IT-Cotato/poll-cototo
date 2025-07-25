24b0b08d462df82147acb50ad566130beac79850
youth <boysoeng@naver.com>
2025-07-26 05:14:59 +0900

    feat: 팀 목록 조회 API 추가

--- src/main/java/org/cotato/poll/polltato/web/controller/TeamController.java
+++ src/main/java/org/cotato/poll/polltato/web/controller/TeamController.java
@@ -1,9 +1,15 @@
 package org.cotato.poll.polltato.web.controller;
 
+import java.util.List;
+
 import org.cotato.poll.polltato.domain.team.service.TeamService;
+import org.cotato.poll.polltato.domain.team.service.dto.TeamDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -12,4 +18,10 @@ import lombok.RequiredArgsConstructor;
 public class TeamController {
 
 	private final TeamService teamService;
+
+	@Operation(summary = "팀 목록 조회 API")
+	@GetMapping
+	public ResponseEntity<List<TeamDto>> getAllTeamsByWorkspaceId(final Long workspaceId) {
+		return ResponseEntity.ok(teamService.getAllTeamsByWorkspaceId(workspaceId));
+	}
 }
