5a8236cb5f37fc22a3bb50a22cebf578fc3cf9b2
youth <boysoeng@naver.com>
2025-07-26 03:54:25 +0900

    feat: swagger issue 해결

--- src/main/java/org/cotato/poll/polltato/global/config/SwaggerConfig.java
+++ src/main/java/org/cotato/poll/polltato/global/config/SwaggerConfig.java
@@ -2,11 +2,19 @@ package org.cotato.poll.polltato.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${app.base-url:http://localhost:8080}")
+    private String baseUrl;
 
     @Bean
     public OpenAPI openAPI() {
@@ -14,6 +22,12 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Polltato API")
                         .description("Polltato 투표 서비스 API 문서")
-                        .version("v1.0.0"));
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("Polltato Team")
+                                .email("support@polltato.com")))
+                .servers(List.of(
+                        new Server().url(baseUrl).description("서버 URL")
+                ));
     }
 } 
