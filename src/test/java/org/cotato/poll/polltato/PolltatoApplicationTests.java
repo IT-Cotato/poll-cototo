d6359b147302b19f75fb7915f9a71d7f47c7fc01
youth <boysoeng@naver.com>
2025-07-25 19:38:27 +0900

    initial commit

---
+++ src/test/java/org/cotato/poll/polltato/PolltatoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cotato.poll.polltato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolltatoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
