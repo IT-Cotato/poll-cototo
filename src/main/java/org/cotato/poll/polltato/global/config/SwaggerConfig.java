575444f046f3b54260854d2849ef98eaf043387d
youth <boysoeng@naver.com>
2025-07-26 05:15:58 +0900

    chore: swagger-base url

--- src/main/java/org/cotato/poll/polltato/global/config/SwaggerConfig.java
+++ src/main/java/org/cotato/poll/polltato/global/config/SwaggerConfig.java
@@ -1,33 +1,34 @@
 package org.cotato.poll.polltato.global.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Value("${app.base-url:http://localhost:8080}")
-    private String baseUrl;
+	@Value("${swagger.base.url:http://localhost:8080}")
+	private String baseUrl;
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Polltato API")
-                        .description("Polltato 투표 서비스 API 문서")
-                        .version("v1.0.0")
-                        .contact(new Contact()
-                                .name("Polltato Team")
-                                .email("support@polltato.com")))
-                .servers(List.of(
-                        new Server().url(baseUrl).description("서버 URL")
-                ));
-    }
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("Polltato API")
+				.description("Polltato 투표 서비스 API 문서")
+				.version("v1.0.0")
+				.contact(new Contact()
+					.name("Polltato Team")
+					.email("support@polltato.com")))
+			.servers(List.of(
+				new Server().url(baseUrl).description("서버 URL")
+			));
+	}
 } 
